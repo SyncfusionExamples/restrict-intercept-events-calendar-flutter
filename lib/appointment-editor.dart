bb61f042c64b8b92a132e24c545e8d6779cf0cd4
Nijamudeen Mohamed Sulaiman <nijamudeensulaiman@syncfusion.com>
2020-04-15 11:12:23 +0530

    Sample added

---
+++ lib/appointment-editor.dart
@@ -0,0 +1,435 @@
+part of event_calendar;
+
+class AppointmentEditor extends StatefulWidget {
+  @override
+  AppointmentEditorState createState() => AppointmentEditorState();
+}
+
+class AppointmentEditorState extends State<AppointmentEditor> {
+
+  Widget _getAppointmentEditor(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: <Widget>[
+            ListTile(
+              contentPadding: const EdgeInsets.fromLTRB(5, 0, 5, 5),
+              leading: const Text(''),
+              title: TextField(
+                controller: TextEditingController(text: _subject),
+                onChanged: (String value) {
+                  _subject = value;
+                },
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Add title',
+                ),
+              ),
+            ),
+            const Divider(
+              height: 1.0,
+              thickness: 1,
+            ),
+            ListTile(
+                contentPadding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
+                leading: Icon(
+                  Icons.access_time,
+                  color: Colors.black54,
+                ),
+                title: Row(children: <Widget>[
+                  const Expanded(
+                    child: Text('All-day'),
+                  ),
+                  Expanded(
+                      child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Switch(
+                            value: _isAllDay,
+                            onChanged: (bool value) {
+                              setState(() {
+                                _isAllDay = value;
+                              });
+                            },
+                          ))),
+                ])),
+            ListTile(
+                contentPadding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
+                leading: const Text(''),
+                title: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 7,
+                        child: GestureDetector(
+                            child: Text(
+                                DateFormat('EEE, MMM dd yyyy')
+                                    .format(_startDate),
+                                textAlign: TextAlign.left),
+                            onTap: () async {
+                              final DateTime date = await showDatePicker(
+                                context: context,
+                                initialDate: _startDate,
+                                firstDate: DateTime(1900),
+                                lastDate: DateTime(2100),
+                              );
+                                if (date != null && date != _startDate) {
+                                setState(() {
+                                  final Duration difference =
+                                      _endDate.difference(_startDate);
+                                  _startDate = DateTime(
+                                      date.year,
+                                      date.month,
+                                      date.day,
+                                      _startTime.hour,
+                                      _startTime.minute,
+                                      0);
+                                  _endDate = _startDate.add(difference);
+                                  _endTime = TimeOfDay(
+                                      hour: _endDate.hour,
+                                      minute: _endDate.minute);
+                                });
+                              }
+                            }),
+                      ),
+                      Expanded(
+                          flex: 3,
+                          child: _isAllDay
+                              ? const Text('')
+                              : GestureDetector(
+                                  child: Text(
+                                    DateFormat('hh:mm a').format(_startDate),
+                                    textAlign: TextAlign.right,
+                                  ),
+                                  onTap: () async {
+                                    final TimeOfDay time = await showTimePicker(
+                                        context: context,
+                                        initialTime: TimeOfDay(
+                                            hour: _startTime.hour,
+                                            minute: _startTime.minute));
+
+                                    if (time != null && time != _startTime) {
+                                      setState(() {
+                                        _startTime = time;
+                                        final Duration difference =
+                                            _endDate.difference(_startDate);
+                                        _startDate = DateTime(
+                                            _startDate.year,
+                                            _startDate.month,
+                                            _startDate.day,
+                                            _startTime.hour,
+                                            _startTime.minute,
+                                            0);
+                                        _endDate = _startDate.add(difference);
+                                        _endTime = TimeOfDay(
+                                            hour: _endDate.hour,
+                                            minute: _endDate.minute);
+                                      });
+                                    }
+                                  })),
+                    ])),
+            ListTile(
+                contentPadding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
+                leading: const Text(''),
+                title: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 7,
+                        child: GestureDetector(
+                            child: Text(
+                              DateFormat('EEE, MMM dd yyyy').format(_endDate),
+                              textAlign: TextAlign.left,
+                            ),
+                            onTap: () async {
+                              final DateTime date = await showDatePicker(
+                                context: context,
+                                initialDate: _endDate,
+                                firstDate: DateTime(1900),
+                                lastDate: DateTime(2100),
+                              );
+                                if (date != null && date != _endDate) {
+                                setState(() {
+                                  final Duration difference =
+                                      _endDate.difference(_startDate);
+                                  _endDate = DateTime(
+                                      date.year,
+                                      date.month,
+                                      date.day,
+                                      _endTime.hour,
+                                      _endTime.minute,
+                                      0);
+                                  if (_endDate.isBefore(_startDate)) {
+                                    _startDate = _endDate.subtract(difference);
+                                    _startTime = TimeOfDay(
+                                        hour: _startDate.hour,
+                                        minute: _startDate.minute);
+                                  }
+                                });
+                              }
+                            }),
+                      ),
+                      Expanded(
+                          flex: 3,
+                          child: _isAllDay
+                              ? const Text('')
+                              : GestureDetector(
+                                  child: Text(
+                                    DateFormat('hh:mm a').format(_endDate),
+                                    textAlign: TextAlign.right,
+                                  ),
+                                  onTap: () async {
+                                    final TimeOfDay time = await showTimePicker(
+                                        context: context,
+                                        initialTime: TimeOfDay(
+                                            hour: _endTime.hour,
+                                            minute: _endTime.minute));
+
+                                    if (time != null && time != _endTime) {
+                                      setState(() {
+                                        _endTime = time;
+                                        final Duration difference =
+                                            _endDate.difference(_startDate);
+                                        _endDate = DateTime(
+                                            _endDate.year,
+                                            _endDate.month,
+                                            _endDate.day,
+                                            _endTime.hour,
+                                            _endTime.minute,
+                                            0);
+                                        if (_endDate.isBefore(_startDate)) {
+                                          _startDate =
+                                              _endDate.subtract(difference);
+                                          _startTime = TimeOfDay(
+                                              hour: _startDate.hour,
+                                              minute: _startDate.minute);
+                                        }
+                                      });
+                                    }
+                                  })),
+                    ])),
+            ListTile(
+              contentPadding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
+              leading: Icon(
+                Icons.public,
+                color: Colors.black87,
+              ),
+              title: Text(_timeZoneCollection[_selectedTimeZoneIndex]),
+              onTap: () {
+                showDialog<Widget>(
+                  context: context,
+                  barrierDismissible: true,
+                  builder: (BuildContext context) {
+                    return _TimeZonePicker();
+                  },
+                ).then((dynamic value) => setState(() {}));
+              },
+            ),
+            const Divider(
+              height: 1.0,
+              thickness: 1,
+            ),
+            ListTile(
+              contentPadding: const EdgeInsets.fromLTRB(5, 2, 5, 2),
+              leading: Icon(Icons.lens,
+                  color: _colorCollection[_selectedColorIndex]),
+              title: Text(
+                _colorNames[_selectedColorIndex],
+              ),
+              onTap: () {
+                showDialog<Widget>(
+                  context: context,
+                  barrierDismissible: true,
+                  builder: (BuildContext context) {
+                    return _ColorPicker();
+                  },
+                ).then((dynamic value) => setState(() {}));
+              },
+            ),
+            const Divider(
+              height: 1.0,
+              thickness: 1,
+            ),
+            ListTile(
+              contentPadding: const EdgeInsets.all(5),
+              leading: Icon(
+                Icons.subject,
+                color: Colors.black87,
+              ),
+              title: TextField(
+                controller: TextEditingController(text: _notes),
+                onChanged: (String value) {
+                  _notes = value;
+                },
+                keyboardType: TextInputType.multiline,
+                maxLines: null,
+                style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black87,
+                    fontWeight: FontWeight.w400),
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Add description',
+                ),
+              ),
+            ),
+            const Divider(
+              height: 1.0,
+              thickness: 1,
+            ),
+          ],
+        ));
+  }
+
+  @override
+  Widget build([BuildContext context]) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text(getTile()),
+              backgroundColor: _colorCollection[_selectedColorIndex],
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.close,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              actions: <Widget>[
+                IconButton(
+                    padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                    icon: const Icon(
+                      Icons.done,
+                      color: Colors.white,
+                    ),
+                    onPressed: () async{
+                      final Meeting startTimeAppointment = _isInterceptExistingAppointments(_startDate, _selectedAppointment);
+                      final Meeting endTimeAppointment = _isInterceptExistingAppointments(_endDate, _selectedAppointment);
+
+                      AlertDialog alert;
+                      if(startTimeAppointment != null || endTimeAppointment != null) {
+                        Widget okButton = FlatButton(
+                          child: Text("Ok"),
+                          onPressed: () {
+                            Navigator.pop(context, true);
+                          },
+                        );
+                        alert = AlertDialog(
+                          title: Text("Alert"),
+                          content: Text(
+                              'Have intercept with existing'),
+                          actions: [
+                            okButton,
+                          ],
+                        );
+
+                        await showDialog<bool>(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return alert;
+                          },
+                        );
+
+                        return;
+                      }
+
+                      final List<Meeting> meetings = <Meeting>[];
+                      if (_selectedAppointment != null) {
+                        events.appointments.removeAt(
+                            events.appointments.indexOf(_selectedAppointment));
+                        events.notifyListeners(CalendarDataSourceAction.remove,
+                            <Meeting>[]..add(_selectedAppointment));
+                      }
+                      meetings.add(Meeting(
+                        from: _startDate,
+                        to: _endDate,
+                        background: _colorCollection[_selectedColorIndex],
+                        startTimeZone: _selectedTimeZoneIndex == 0
+                            ? ''
+                            : _timeZoneCollection[_selectedTimeZoneIndex],
+                        endTimeZone: _selectedTimeZoneIndex == 0
+                            ? ''
+                            : _timeZoneCollection[_selectedTimeZoneIndex],
+                        description: _notes,
+                        isAllDay: _isAllDay,
+                        eventName: _subject == '' ? '(No title)' : _subject,
+                      ));
+
+                      events.appointments.add(meetings[0]);
+
+                      events.notifyListeners(
+                          CalendarDataSourceAction.add, meetings);
+                      _selectedAppointment = null;
+
+                      Navigator.pop(context);
+                    })
+              ],
+            ),
+            body: Padding(
+              padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+              child: Stack(
+                children: <Widget>[_getAppointmentEditor(context)],
+              ),
+            ),
+            floatingActionButton: _selectedAppointment == null
+                ? const Text('')
+                : FloatingActionButton(
+                    onPressed: () {
+                      if (_selectedAppointment != null) {
+                        events.appointments.removeAt(
+                            events.appointments.indexOf(_selectedAppointment));
+                        events.notifyListeners(CalendarDataSourceAction.remove,
+                            <Meeting>[]..add(_selectedAppointment));
+                        _selectedAppointment = null;
+                        Navigator.pop(context);
+                      }
+                    },
+                    child:
+                        const Icon(Icons.delete_outline, color: Colors.white),
+                    backgroundColor: Colors.red,
+                  )));
+  }
+
+  String getTile() {
+    return _subject.isEmpty ? 'New event' : 'Event details';
+  }
+
+  dynamic _isInterceptExistingAppointments(DateTime date, Meeting selectedAppointment) {
+    if(date == null ||events ==null || events.appointments == null || events.appointments.isEmpty)
+      return null;
+    for (int i = 0; i < events.appointments.length; i++) {
+      var appointment = events.appointments[i];
+      if (appointment != selectedAppointment && (date.isAfter(appointment.from) || _isSameDateTime(date, appointment.from)) && date.isBefore(appointment.to)) {
+        return appointment;
+      }
+    }
+    return null;
+  }
+
+  bool _isSameDateTime(DateTime date1, DateTime date2) {
+    if (date1 == date2) {
+      return true;
+    }
+
+    if (date1 == null || date2 == null) {
+      return false;
+    }
+
+    if (date1.year == date2.year && date1.month == date2.month &&
+        date1.day == date2.day && date1.hour == date2.hour &&
+        date1.minute == date2.minute) {
+      return true;
+    }
+
+    return false;
+  }
+}
